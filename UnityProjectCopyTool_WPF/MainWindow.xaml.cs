260991a26d5e75ca31a5192d044dd72a220aec41
DESKTOP-NS\NShome <sounaka0203@ezweb.ne.jp>
2023-04-17 23:38:06 +0900

    Update:画面レイアウト、最前面表示機能、フォルダー選択ダイアログの表示

--- UnityProjectCopyTool_WPF/MainWindow.xaml.cs
+++ UnityProjectCopyTool_WPF/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace UnityProjectCopyTool_WPF
 {
@@ -25,7 +28,27 @@ namespace UnityProjectCopyTool_WPF
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void AlwaysOnTopCheckBox_Clicked(object sender, RoutedEventArgs e)
+        {
+            // 未確定時(null)はやめる
+            if (AlwaysOnTopCheckBox.IsChecked == null) return;
+            Topmost = (bool)AlwaysOnTopCheckBox.IsChecked;
+        }
+
+        private void OpenFolderDialogButton_Click(object sender, RoutedEventArgs e)
+        {
+            // 参考記事
+            // https://threeshark3.com/commonopenfiledialog/
+            using (var cofd = new CommonOpenFileDialog()
+            { Title = "フォルダを選択してください", IsFolderPicker = true })
+            {
+                if (cofd.ShowDialog() != CommonFileDialogResult.Ok) return;
+                // 選択されたフォルダ名を保持
+                SelectFolderAddressTextBox.Text = cofd.FileName;
+            }
+        }
+
+        private void CopyButton_Click(object sender, RoutedEventArgs e)
         {
 
         }
