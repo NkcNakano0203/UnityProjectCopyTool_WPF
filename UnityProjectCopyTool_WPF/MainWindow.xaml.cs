ae368af7848cb48535dfe44448993a2ce6bd963e
Nkn_nkc <nkc.souki.nakano@gmail.com>
2023-04-21 12:27:50 +0900

    UIを改善

--- UnityProjectCopyTool_WPF/MainWindow.xaml.cs
+++ UnityProjectCopyTool_WPF/MainWindow.xaml.cs
@@ -1,19 +1,6 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using Microsoft.Win32;
 using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace UnityProjectCopyTool_WPF
